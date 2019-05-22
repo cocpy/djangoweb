69063a6b2d021041f6c819d97417882a22fb7d70
lizaitian <15754324003@163.com>
2019-05-22 16:06:50 +0800

    Initial commit

---
+++ djangoweb/urls.py
@@ -0,0 +1,24 @@
+"""djangoweb URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('log/', include(('apps.log.urls', 'apps.log'), namespace='log')),  # 日志配置
+    path('mail/', include(('apps.mail.urls', 'apps.mail'), namespace='mail')),  # 邮件验证模块
+]
